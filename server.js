e69836abdfb3f88c1e12f524d861878590f39106
Jerry Huang <Jerry.Huang@dkdtrillion.com>
2022-04-18 00:35:31 +0800

    完成 get posts

--- server.js
+++ server.js
@@ -9,7 +9,32 @@ mongoose
     .then(() => console.log('資料庫連接成功'))
 
 const requestListener = async (req, res) => {
-
+    if (req.url == '/posts' && req.method == 'GET') {
+        const posts = await Post.find();
+        res.writeHead(200, HEADERS);
+        res.write(JSON.stringify({
+            "status": "success",
+            data: posts
+        }));
+        res.end();
+    } else if (req.url.startsWith('/posts?keyword=') && req.method == 'GET') {
+        console.log(req)
+        const keyword = req.url.split('=').pop();
+        // 編碼處理
+        const newKeyword = decodeURI(keyword)
+        const posts = await Post.find({
+            content: {
+                $regex: newKeyword
+            }
+        });
+        console.log(posts)
+        res.writeHead(200, HEADERS);
+        res.write(JSON.stringify({
+            "status": "success",
+            data: posts
+        }));
+        res.end();
+    }
 }
 
 const server = http.createServer(requestListener)
