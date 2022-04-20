8517aadb65047c3a2bec8972dbee7db1d7856ab0
hff2 <hophoebeho@gmail.com>
2022-04-20 23:15:22 +0800

    fix: fix the createdAt error

--- models/post.js
+++ models/post.js
@@ -21,7 +21,7 @@ const postSchema = new mongoose.Schema(
         createdAt: {
             type: Date,
             default: Date.now(),
-            select: false
+            select: true
         },
     },
     {
